f03612e292eab377a3eae68ba482aeddb2621dbc
salsyns <ical501nc@gmail.com>
2023-12-08 19:24:01 +0700

    Tugas Pemrograman Web

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.style.display = (menu.style.display === "none") ? "block" : "none";
 }
+
+function toggleBs() {
+    var menu = document.getElementById("bs");
+    menu.style.display = (menu.style.display === "none") ? "block" : "none";
+}
